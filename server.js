3979c1968c0cbc37e0c1bebc276de76c476ff1e0
jannunzi <jannunzi@gmail.com>
2021-10-31 09:23:52 -0400

    GET ALL twitter REST API

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin",
+      "*");
+  res.header("Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 require('./services/tweets-service')(app);
+require('./services/todos-service')(app);
 
 app.get('/hello', (req, res) => {
   res.send('Hello World!');
