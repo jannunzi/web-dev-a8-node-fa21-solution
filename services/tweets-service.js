95103066c90287a934a4bb00c99392515524e944
jannunzi <jannunzi@gmail.com>
2021-10-31 10:17:22 -0400

    posting new tweets

--- services/tweets-service.js
+++ services/tweets-service.js
@@ -1,4 +1,4 @@
-const tweets = require('../data/tweets.json');
+let tweets = require('../data/tweets.json');
 
 module.exports = (app) => {
     
@@ -6,5 +6,31 @@ module.exports = (app) => {
         res.json(tweets);
     }
     
+    const createTweet = (req, res) => {
+        const newTweet = {
+            _id: (new Date()).getTime() + '',
+            "topic": "Web Development",
+            "userName": "ReactJS",
+            "verified": false,
+            "handle": "ReactJS",
+            "time": "2h",
+            "avatar-image": "../../../images/react-blue.png",
+            "logo-image": "../../../images/react-blue.png",
+            "stats": {
+                "comments": 123,
+                "retweets": 234,
+                "likes": 345
+            },
+            ...req.body,
+        }
+        newTweet['_id'] = (new Date()).getTime();
+        tweets = [
+            newTweet,
+            ...tweets
+        ];
+        res.json(newTweet);
+    }
+
+    app.post('/api/tweets', createTweet);
     app.get('/api/tweets', findAllTweets);
 };
