128f3dbf73f77926c7435d8d1abc3116bb30273e
jannunzi <jannunzi@gmail.com>
2021-10-31 20:25:59 -0400

    like tweets

--- services/tweets-service.js
+++ services/tweets-service.js
@@ -36,6 +36,27 @@ module.exports = (app) => {
         tweets = tweets.filter(tweet => tweet._id !== id);
         res.sendStatus(200);
     }
+    
+    const likeTweet = (req, res) => {
+        const id = req.params['id'];
+        tweets = tweets.map(tweet => {
+            if (tweet._id === id) {
+                if (tweet.liked === true) {
+                    tweet.liked = false;
+                    tweet.stats.likes--;
+                } else {
+                    tweet.liked = true;
+                    tweet.stats.likes++;
+                }
+                return tweet;
+            } else {
+                return tweet;
+            }
+        });
+        res.sendStatus(200);
+    }
+    
+    app.put('/api/tweets/:id/like', likeTweet);
     app.delete('/api/tweets/:id', deleteTweet);
     app.post('/api/tweets', createTweet);
     app.get('/api/tweets', findAllTweets);
